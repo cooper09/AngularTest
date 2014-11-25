f1bf2149ab8ca032d72ecd429acdefeaf54eddc7
cooper09 <cooper09@Biggie-II.local>
2014-11-24 22:06:53 -0500

    init my little testy world

---
+++ app.js
@@ -0,0 +1,11 @@
+var testApp = angular.module('testApp', []);
+
+testApp.controller('testCtrl', function($scope, $http) {
+
+var testData;
+
+$scope.init = function () {
+ console.log("And away we go....!");
+}
+
+});//end controller
